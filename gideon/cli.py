83bf5c5dc6cc4138e12048cfeb0ae3d9ec051e50
Axel Go <j.axelgo24@gmail.com>
2026-07-10 01:59:51 +0800

    Refractor command handling

--- gideon/cli.py
+++ gideon/cli.py
@@ -1,13 +1,45 @@
 import typer
 
-from gideon.ui import show_welcome
-
+from gideon.ui import (
+    console, 
+    show_help, 
+    show_welcome,
+    show_version,
+    refresh_dashboard
+)
 app = typer.Typer()
 
 
-@app.callback(invoke_without_command=True)
+commands = {
+    "help": show_help,
+    "version": show_version,
+    "clear": refresh_dashboard,
+
+
+} 
+
+@app.callback(invoke_without_command=True) 
+
 def main() -> None:
+    """Start Gideon."""
+
     show_welcome()
+
+    while True:
+        command = input("> ").strip().lower()
+
+       
+        if command == "exit":
+            print("til next time!")
+            break
+
+        elif command in commands:
+            commands[command]()
+            
+            
+        else:
+            print(f"Unknown command: {command}")
+
 
 
 if __name__ == "__main__":
--- gideon/ui.py
+++ gideon/ui.py
@@ -20,6 +20,27 @@ def show_welcome() -> None:
             f"Python: {get_python_version()}\n"
             f"Branch: {get_git_branch()}\n\n"
             "Hello Axel, What can i do for you today?",
-            title="v0.1.0",
+            title="v0.3.0",
         )
     )
+
+def show_help() -> None:
+    """Display available commands."""
+
+    console.print("[bold cyan]Available Commands[/bold cyan]\n")
+
+    console.print("  help     Show this help menu")
+    console.print("  version  Show Gideon's version")
+    console.print("  clear    Clear the screen")
+    console.print("  exit     Exit Gideon")
+
+def show_version() -> None:
+    """Display Gideon's current version"""
+
+    console.print("v0.3.0")
+
+def refresh_dashboard() -> None:
+    """Refresh Dashboard"""
+
+    console.clear()
+    show_welcome()
