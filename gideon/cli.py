6a6adf882cfb32cd6443ef0ffec6cc263bb0c530
Axel Go <j.axelgo24@gmail.com>
2026-07-18 13:41:45 +0800

    Implemented configuration with set command

--- gideon/cli.py
+++ gideon/cli.py
@@ -1,5 +1,8 @@
 import typer
-from gideon.config import load_config
+from gideon.config import (
+    load_config,
+    save_config
+)
 from gideon.system import(
     get_project_name,
     get_python_version,
@@ -17,9 +20,40 @@ from gideon.ui import (
 app = typer.Typer()
 
 
+def set_theme():
+    config_data = load_config()
+
+    config_data["theme"] = "cyberpunk"
+
+    save_config(config_data)
+    print("Theme updated.")
+
+
+
 def config():
     config_data = load_config()
     show_config(config_data)
+
+def set_config(setting, value):
+    valid_settings = [
+        "theme",
+        "provider",
+        "model",
+        "debug",
+        "history"
+    ]
+
+    if setting not in valid_settings:
+        print(f"Unknown setting: {setting}")
+        return
+    config_data = load_config()
+
+    
+    config_data[setting] = value
+
+    save_config(config_data)
+
+    print(f"{setting} updated to {value}.")
 
 
 
@@ -28,7 +62,8 @@ commands = {
     "version": show_version,
     "clear": refresh_dashboard,
     "about": show_about,
-    "config": config
+    "config": config,
+    "theme": set_theme
 
 } 
 
@@ -40,14 +75,29 @@ def main() -> None:
     show_welcome()
 
     while True:
-        command = input("> ").strip().lower()
+        user_input = input("> ").strip().lower()
+        parts = user_input.split()
 
+        if not parts:
+            continue
+
+        command = parts[0]
        
+
         if command == "exit":
             print("til next time!")
             break
-        
 
+        
+        elif command == "set":
+            if len(parts) !=3:
+                print("Usage: set <setting> <value>")
+            else:
+                setting = parts[1]
+                value = parts[2]
+                set_config(setting, value)
+        
+        
         elif command in commands:
             commands[command]()
             
@@ -59,3 +109,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     app()
+
--- gideon/config.py
+++ gideon/config.py
@@ -8,3 +8,9 @@ def load_config() -> dict:
         config = json.load(file)
 
     return config
+
+def save_config(config_data) -> None:
+    """Save Gideon's configuration."""
+
+    with open("config.json","w") as file:
+        json.dump(config_data, file, indent=4)
